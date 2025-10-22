d41131eb4d9888471efeb322cfc0f18a8581633e
beyzakca <98107277+beyzakca@users.noreply.github.com>
2025-10-22 17:51:39 +0300

    Add files via upload

---
+++ SQL_ODEV7.sql
@@ -0,0 +1,18 @@
+SELECT rating, COUNT(*)
+FROM film
+GROUP BY rating; 
+
+SELECT replacement_cost, COUNT(*)
+FROM film
+GROUP BY replacement_cost 
+HAVING COUNT(*) > 50;    
+
+SELECT store_id, COUNT(*) 
+FROM customer
+GROUP BY store_id;           
+
+SELECT country_id , COUNT(city) AS city_count
+FROM city
+GROUP BY country_id 
+ORDER BY city_count DESC
+LIMIT 1;
